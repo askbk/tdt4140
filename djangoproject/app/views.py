8f32d45debd6ce070babf6acbe0088b7dad9e853
magnram <magnram@stud.ntnu.no>
2019-02-08 00:33:38 +0100

    Lagt til et par css-filer, fonter og et bilde. Har også lagt til et eksempel på hvordan man kan koble til et CSS-dokument med django. Dette ble gjort for å forberede oss til sprint 1

--- djangoproject/app/views.py
+++ djangoproject/app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, get_list_or_404, render
+from django.http import HttpResponse, Http404
+from app.models import Advert
 
+#get_list_or_404() henter liste vha filter
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+def index(request):  #Se urls.py for å se når denne aktiveres
+    all_adverts = get_list_or_404(Advert)
+    context = {
+        'all_adverts': all_adverts
+    }
+    return render(request, 'index.html', context)
+
+def advert(request, id): #Se urls.py for å se når denne aktiveres
+    advert = get_object_or_404(Advert, id=id)
+    context = { #Hvilke variabler vil vi sende til html-dokumentet?
+        'advert': advert
+    }
+    return render(request, 'advert.html', context) #sender besøkende til html-dokumentet
