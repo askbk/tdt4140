205688dce612e32a2312b16eaa333d401ff64186
ask <askbk@protonmail.com>
2019-02-21 13:58:39 +0100

    Byttet navn på test

--- djangoproject/app/tests/test_forms.py
+++ djangoproject/app/tests/test_forms.py
@@ -6,7 +6,7 @@ from app.models import Address, Startup, Tag, Person, Advert
 from django.forms import ModelForm
 
 class UserTest(TestCase):
-    def test_user_creation(self):
+    def test_user_registration(self):
         pw_mismatch = {"username": "Bankchain", "password1": "passord1", "passord2": "passord2"}
         user_creation_form = UserCreationForm(data=pw_mismatch)
         self.assertTrue(not user_creation_form.is_valid())
