b073b46bd7117bf2f9392619649d9178fb7a584a
ask <askbk@protonmail.com>
2019-02-21 13:21:05 +0100

    Fikset anførselstegn nå!

--- djangoproject/app/tests/test_models.py
+++ djangoproject/app/tests/test_models.py
@@ -39,7 +39,8 @@ class TagTest(TestCase):
 class StartupTest(TestCase):
     def create_startup(self):
         return Startup.objects.create(
-            bio="A blockchain startup with the mission of creating a sustainable banking system",
+            bio="""A blockchain startup with the mission of creating a
+                sustainable banking system""",
             user=User.objects.create(
                 username="Bankchain",
                 password="passord1"
@@ -50,6 +51,6 @@ class StartupTest(TestCase):
     def test_startup_creation(self):
         w = self.create_startup()
         self.assertTrue(isinstance(w, Startup))
-        self.assertEqual("A blockchain startup with the mission of creating a sustainable
-            banking system", w.bio)
+        self.assertEqual("""A blockchain startup with the mission of creating a
+            sustainable banking system""", w.bio)
         self.assertEqual("Banking", w.__str__())
