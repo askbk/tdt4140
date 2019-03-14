f1e730aff2fee191b2c7cbd392beece555d3c800
ask <askbk@protonmail.com>
2019-03-14 10:08:32 +0100

    La til kommentar

--- djangoproject/app/static/js/index.js
+++ djangoproject/app/static/js/index.js
@@ -1,5 +1,12 @@
 import { Filter } from "./filter.js";
 
+/*
+*   Filtrerer alle elementer med klasse "content-item" basert på om de har tags
+*   eller fritekst som matcher filteret.
+*
+*   Se filter.js for virkemåten til filterklassen.
+*/
+
 const startupFilter = new Filter(
     true,
     "content-item",
