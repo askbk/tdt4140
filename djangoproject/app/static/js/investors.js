cfaff783747a39b65d03546a3bcd6d88caf757c4
ask <askbk@protonmail.com>
2019-03-14 10:08:53 +0100

    La til kommentar

--- djangoproject/app/static/js/investors.js
+++ djangoproject/app/static/js/investors.js
@@ -1,5 +1,12 @@
 import { Filter } from "./filter.js";
 
+/*
+*   Filtrerer alle elementer med klasse "investor-item" basert på om de har tags
+*   eller fritekst som matcher filteret.
+*
+*   Se filter.js for virkemåten til filterklassen.
+*/
+
 const investorFilter = new Filter(
     true,
     "investor-item",
