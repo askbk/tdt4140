5058a19e73475d780e2210c2a2b5884a46623217
ask <askbk@protonmail.com>
2019-03-14 10:08:06 +0100

    La til kommentar

--- djangoproject/app/static/js/adverts.js
+++ djangoproject/app/static/js/adverts.js
@@ -1,5 +1,12 @@
 import { Filter } from "./filter.js";
 
+/*
+*   Filtrerer alle elementer med klasse "advert-item" basert på om de har tags
+*   eller fritekst som matcher filteret.
+*
+*   Se filter.js for virkemåten til filterklassen.
+*/
+
 const advertFilter = new Filter(
     true,
     "advert-item",
