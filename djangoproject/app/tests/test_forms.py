267212bc65f8df37e83a308b479cadc8d8098fb0
Lars Digerud <you@example.com>
2019-04-08 10:22:11 +0200

    Satt opp test klasser for forms

--- djangoproject/app/tests/test_forms.py
+++ djangoproject/app/tests/test_forms.py
@@ -4,6 +4,27 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from app.models import Address, Startup, Tag, Person, Advert
 from django.forms import ModelForm
+from .forms import *   # import all forms
+
+class Setup_Class(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="usertest@usertest.no", password="user", first_name="user", phone=12345678)
+
+class User_Form_Test(TestCase):
+
+    # Valid Form Data
+    def test_UserForm_valid(self):
+        form = UserForm(data={'email': "usertest@usertest.no", 'password': "user", 'first_name': "user", 'phone': 12345678})
+        self.assertTrue(form.is_valid())
+
+    # Invalid Form Data
+    def test_UserForm_invalid(self):
+        form = UserForm(data={'email': "", 'password': "mp", 'first_name': "mp", 'phone': ""})
+        self.assertFalse(form.is_valid())
+
+
+
 '''
 class UserTest(TestCase):
     def test_user_registration(self):
